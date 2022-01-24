025db9831b252b2d47e4adc8bef47098ae0ade24
gabrieltellez <tellez.gabriel1709@gmail.com>
2022-01-23 18:49:04 -0800

    forecast description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,6 @@
 const request = require('postman-request')
+
+
 const forecast = (longitude, latitude, callback)=>{
     const url = 'http://api.weatherstack.com/current?access_key=841f9518580892b2763bf02423f4f2d6&query='+ latitude+ ','+ longitude +'&units=f'
     request({ url, json: true}, (error, { body })=>{
@@ -8,7 +10,8 @@ const forecast = (longitude, latitude, callback)=>{
             callback('Unable to find location')
         }else{
             const currentInfo = body.current
-            callback(undefined, `${currentInfo.weather_descriptions[0]}.  It is ${currentInfo.temperature} degrees out.  It feels like ${currentInfo.feelslike} degrees out`)
+            let forecastMessage = 
+            callback(undefined, `It is ${currentInfo.weather_descriptions[0]};  ${currentInfo.temperature} degrees out, feels like ${currentInfo.feelslike} degrees out; the humidity is ${currentInfo.humidity}% and wind speeds are at ${currentInfo.wind_speed} mph`)
         }
     })
 }
